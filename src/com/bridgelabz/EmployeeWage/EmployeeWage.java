aa83639cd63b7bf9f179e8b2b2314827d8104252
sumithanvat <skhanvat@gmail.com>
2023-03-03 23:27:04 +0530

    added:Daily_Wage_Part_Time

--- src/com/bridgelabz/EmployeeWage/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage/EmployeeWage.java
@@ -8,15 +8,26 @@ public class EmployeeWage {
 
 
         Random random = new Random();
-        int present = random.nextInt(2);
+        int present = random.nextInt(3);
         //Check if employee is present
-        int WagePerHr =20;
-        int wrkHrs = 8;
-        int dailyWage = present * WagePerHr * wrkHrs;
+        int WagePerHr = 20;
+        int dailyWage, workingHrs;
+        if (present == 0) {
+            //Employee is absent
+            workingHrs = 0;
+            System.out.println("Employee is absent");
+        } else if (present == 1) {
+            //Employee working as part-time
+            workingHrs = 8;
+            System.out.println("Employee working as part-time");
+        } else {
+            //Employee working as full time
+            workingHrs = 16;
+            System.out.println("Employee working as full time");
+        }
+        dailyWage = workingHrs * WagePerHr;
+        System.out.println("Daily wage is " + dailyWage);
 
-        if (present == 1)
-            System.out.println("Employee is present and Daily Wage Is " + dailyWage);
-        else
-            System.out.println("Employee is absent so Daily Wage is "+ dailyWage);
+
     }
 }
