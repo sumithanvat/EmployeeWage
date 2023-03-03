42dd5833e97541ce74405e818abf234f32cdf1a7
sumithanvat <skhanvat@gmail.com>
2023-03-03 23:39:09 +0530

    added:Employee_Wage_Switch

--- src/com/bridgelabz/EmployeeWage/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage/EmployeeWage.java
@@ -3,6 +3,9 @@ package com.bridgelabz.EmployeeWage;
 import java.util.Random;
 
 public class EmployeeWage {
+    private static final int FULL_TIME = ;
+    private static final int PART_TIME = ;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Problem");
 
@@ -12,18 +15,18 @@ public class EmployeeWage {
         //Check if employee is present
         int WagePerHr = 20;
         int dailyWage, workingHrs;
-        if (present == 0) {
-            //Employee is absent
-            workingHrs = 0;
-            System.out.println("Employee is absent");
-        } else if (present == 1) {
-            //Employee working as part-time
-            workingHrs = 8;
-            System.out.println("Employee working as part-time");
-        } else {
-            //Employee working as full time
-            workingHrs = 16;
-            System.out.println("Employee working as full time");
+        switch (present){
+            case FULL_TIME :
+                workingHrs = 8;
+                System.out.println("Employee Working Fullday");
+                break;
+            case PART_TIME:
+                workingHrs =4;
+                System.out.println("Employee Working Part Time");
+                break;
+            default:
+                workingHrs = 0;
+                System.out.println("Employee Absent");
         }
         dailyWage = workingHrs * WagePerHr;
         System.out.println("Daily wage is " + dailyWage);
