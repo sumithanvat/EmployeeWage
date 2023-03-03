453466a3845f9ea5e6305e50e24aef8430353075
sumithanvat <skhanvat@gmail.com>
2023-03-03 23:14:33 +0530

    added:EmployeeWage

--- src/com/bridgelabz/EmployeeWage/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage/EmployeeWage.java
@@ -7,12 +7,16 @@ public class EmployeeWage {
         System.out.println("Welcome to Employee Wage Computation Problem");
 
 
-    Random random = new Random();
-    int present = random.nextInt(2);
-//Check if employee is present
+        Random random = new Random();
+        int present = random.nextInt(2);
+        //Check if employee is present
+        int WagePerHr =20;
+        int wrkHrs = 8;
+        int dailyWage = present * WagePerHr * wrkHrs;
+
         if (present == 1)
-                System.out.println("Employee is present.");
-                else
-                System.out.println("Employee is absent.");
-                }
-                }
+            System.out.println("Employee is present and Daily Wage Is " + dailyWage);
+        else
+            System.out.println("Employee is absent so Daily Wage is "+ dailyWage);
+    }
+}
